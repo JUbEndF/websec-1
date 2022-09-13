ca77a7a0dd9a384d163d1f34400293a4dba22910
Георгий Бурнаев <burnaev_01@mail.ru>
2022-09-13 10:01:11 +0400

    All done

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 function buttonClickHandler(){
 	let selectElem = document.getElementById("select1");
 
-	let value1 = parseFloat(document.getElementById("input1").value);
-	let value2 = parseFloat(document.getElementById("input2").value);
+	let value1 = document.getElementById("input1").value;
+	let value2 = document.getElementById("input2").value;
+	
+	if(value1 != /[+-]?([0-9]*[.])?[0-9]+/ || value2 != /[+-]?([0-9]*[.])?[0-9]+/)
+	{
+		let results = "Incorrect values entered";
+		console.log(results);
+		document.getElementById("results").innerText = results;
+		return;
+	}
 
+	value1 = parseFloat(value1);
+    value2 = parseFloat(value2);
+	
 	console.log(value1);
 	console.log(value2);
-
 
 	let result = 0.0;
 	switch(selectElem.value){
 		case "+":
-		result = value1 + value2;
-		break;
+			result = value1 + value2;
+			break;
 		case "-":
-		result = value1 - value2;
-		break;
+			result = value1 - value2;
+			break;
 		case "*":
-		result = value1 * value2;
-		break;
+			result = value1 * value2;
+			break;
 		case "/":
-		result = value1 / value2;
-		break;
+			if(value2 !== 0){
+				result = value1 / value2;
+			}
+			else{
+				result = "Error: division by 0 is not possible";
+			}
+			break;
 	}
 	console.log(result);
 	document.getElementById("results").innerText = result;
